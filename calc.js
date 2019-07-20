fd92ca79a843a2aaf9e4f009e7c5a3235c4bf903
Atul <techie20th@gmail.com>
2019-07-20 12:27:06 +0530

    Completed Substract function

---
+++ calc.js
@@ -0,0 +1,9 @@
+let addNumber = function(a = number, b = number) {
+  if (!isNaN(a) && !isNaN(b)) return a + b;
+  return NaN;
+};
+
+let subtractNumber = function() {
+  if (!isNaN(a) && !isNaN(b)) return a - b;
+  return NaN;
+};
